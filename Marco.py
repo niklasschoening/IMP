b24692fb8af710783b83542256876dc09989ca0b
Replit user <>
2022-11-17 14:46:16 +0000

    pandas

---
+++ Marco.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame(
+  {
+    "Länge Fenser": [1000, 2000, 3000, 4000],
+    "gesparter Speicher in %": [25, 53, 76, 12]
+  }
+)
+print(df)
